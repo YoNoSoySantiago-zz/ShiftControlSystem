f68aa531533af765e9b5e894c9158bba4e6f8dee
Santiago Hurtado Solis <guapicito723@gmail.com>
2020-02-08 22:38:35 -0500

    finishDay

--- src/model/ShiftControler.java
+++ src/model/ShiftControler.java
@@ -13,11 +13,11 @@ public class ShiftControler {
 		userShift = new ArrayList<User>();
 		shift = new Shift('A',0,"A00",false);
 	}
-	public void assignShift(String documentNumber) throws UserAlreadyHasShiftException {
+	public void assignShift(String documentNumber,String documentType) throws UserAlreadyHasShiftException {
 		for(int j =0;j<userShift.size();j++) {
 			int letterUser = (int)userShift.get(j).getShift().getLetter();
 			int letterShift = (int)shift.getLetter();
-			if(userShift.get(j).getDocumentNumber()==(documentNumber) && letterShift>= letterUser) {
+			if(userShift.get(j).getDocumentNumber()==(documentNumber) && letterShift>= letterUser && userShift.get(j).getDocumentType()==documentType) {
 				if(letterShift== letterUser) {
 					if(shift.getNumber()>userShift.get(j).getShift().getNumber()) {
 						throw new UserAlreadyHasShiftException();
@@ -29,7 +29,7 @@ public class ShiftControler {
 			}
 		}
 		for(int i = 0;i<users.size();i++) {
-			if(users.get(i).getDocumentNumber()==(documentNumber)) {
+			if(users.get(i).getDocumentNumber()==(documentNumber)&&users.get(i).getDocumentType()==documentType) {
 				User user = users.get(i);
 				user.setShift(generateNextShift());
 				userShift.add(user);
@@ -38,17 +38,22 @@ public class ShiftControler {
 	}
 	//This method is to generate the next Shift in the list 
 	public Shift generateNextShift() {
-		Shift shift = userShift.get(userShift.size()-1).getShift();
-		shift.setNumber(shift.getNumber()+1);
-		if(shift.getNumber()>99) {
-			shift.setNumber(0);
-			shift.setLetter((char) (shift.getLetter()+1));
-		}
-		if(shift.getNumber()<10) {
-			shift.setShift((char)(shift.getLetter())+ "0"+Integer.toString(shift.getNumber()).toUpperCase()); 
+		if(userShift.size()>0) {
+			Shift shift = userShift.get(userShift.size()-1).getShift();
+			shift.setNumber(shift.getNumber()+1);
+			if(shift.getNumber()>99) {
+				shift.setNumber(0);
+				shift.setLetter((char) (shift.getLetter()+1));
+			}
+			if(shift.getNumber()<10) {
+				shift.setShift((char)(shift.getLetter())+ "0"+Integer.toString(shift.getNumber()).toUpperCase()); 
+			}
+			
+			return shift;
+		}else {
+			return shift;
 		}
 		
-		return shift;
 	}
 	
 	public void registerUser(String name,String lastName,String documentType,String documentNumber,String locate,String numberPhone) throws IdUserExistException,ValueIsEmptyException {
@@ -60,21 +65,30 @@ public class ShiftControler {
 		if(name.isEmpty()||lastName.isEmpty()||documentType.isEmpty()||documentNumber.isEmpty()) {
 			throw new ValueIsEmptyException();
 		}
+		if(locate.isEmpty()) {
+			locate = User.UNKNOWN;
+		}
+		if(numberPhone.isEmpty()) {
+			numberPhone = User.UNKNOWN;
+		}
 		users.add(new User(name,lastName,documentType,documentNumber,locate,numberPhone,null));
 	}
-	public void advanceShift() {
-		int letterUser = (int)userShift.get(userShift.size()-1).getShift().getLetter();
-		int letterShift = (int)shift.getLetter();
-		if(letterShift>= letterUser) {
-			if(letterShift== letterUser) {
-				if(shift.getNumber()==userShift.get(userShift.size()-1).getShift().getNumber()) {
-					
+	public void advanceShift() throws NoMoreShiftException {
+		if(userShift.size()>0) {
+			int letterUser = (int)userShift.get(userShift.size()-1).getShift().getLetter();
+			int letterShift = (int)shift.getLetter();
+			if(letterShift>= letterUser) {
+				if(letterShift== letterUser) {
+					if(shift.getNumber()==userShift.get(userShift.size()-1).getShift().getNumber()) {
+						throw new NoMoreShiftException();
+					}
 				}
-			}else{
 				
 			}
-			
+		}else {
+			throw new NoMoreShiftException();
 		}
+		
 		shift.setNumber(shift.getNumber()+1);
 		if(shift.getNumber()>99) {
 			shift.setNumber(0);
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,120 @@
 package ui;
+import model.*;
+import exceptions.*;
+import java.util.Scanner;
 
 public class Main {
-
+	private ShiftControler shiftControler = new ShiftControler(); 
+	public static void main(String[]args) throws IdUserExistException, ValueIsEmptyException {
+		boolean continue1 = true;
+		System.out.println("=============================\nWELCOME\n=============================");
+		Main main = new Main();
+		do {
+			main.showMenu();
+			continue1 = main.start();
+		}while(continue1 == true);
+		
+		
+	}
+	public void showMenu() {
+		System.out.println("1. Agregar usuar\n"+"2. Registrar turno\n"+"3. Atender Turno\n"+"0. Exit");
+	}
+	public boolean start() throws IdUserExistException, ValueIsEmptyException, UserAlreadyHasShiftException, NoMoreShiftException {
+		Scanner s = new Scanner(System.in);
+		boolean continue1= true;
+		int optionAux=0;
+		int option = s.nextInt();
+		String name="",lastName="",documentNumber="",documentType="",locate="",numberPhone="";
+		switch(option) {
+		case 1:
+			System.out.println("Type the name of the user");
+			name=s.next();
+			System.out.println("Type the lastName of the user");
+			lastName=s.next();
+			s.nextLine();
+			System.out.println("Type the documentType of the user: "
+					+ "\n1. citizenship card \n2. civel registration \n3. passport \n4. foreign identity card");
+			optionAux=0;
+			do {
+				optionAux=s.nextInt();
+				s.nextLine();
+				
+				switch(optionAux) {
+				case 1:
+					documentType = User.CC;
+					break;
+				case 2:
+					documentType = User.CR;
+					break;
+				case 3:
+					documentType = User.FIC;
+					break;
+				case 4:
+					documentType = User.PS;
+					break;
+				default:
+					System.out.println("Please type a correctly");
+				}
+			}while(optionAux!=1 &&optionAux!=2 &&optionAux!=3 &&optionAux!=4 );
+			System.out.println("Type the documentNumber of the user");
+			documentNumber=s.next();
+			System.out.println("Want to share your location \n1.Yes \n0.No");
+			optionAux = s.nextInt();
+			
+			if(optionAux ==1) {
+				System.out.println("Type the locate of the user");
+				locate=s.next();
+			
+			}else if(optionAux!=0){
+				System.out.println("this option is not correct so it will become a not automatically");
+			}
+			System.out.println("Want to share your cell/telephone number \n1.Yes \n0.No");
+			optionAux = s.nextInt();
+			if(optionAux ==1) {
+				System.out.println("Type the cell/telephone number of the user");
+				numberPhone=s.next();
+				
+			}else if(optionAux!=0){
+				System.out.println("this option is not correct so it will become a not automatically");
+				
+			}
+			shiftControler.registerUser(name, lastName, documentType, documentNumber, locate, numberPhone);
+			break;
+		case 2:
+			System.out.println("Type the documentType of the user: "
+					+ "\n1. citizenship card \n2. civel registration \n3. passport \n4. foreign identity card");
+			optionAux=0;
+			do {
+				optionAux=s.nextInt();
+				
+				switch(optionAux) {
+				case 1:
+					documentType = User.CC;
+					break;
+				case 2:
+					documentType = User.CR;
+					break;
+				case 3:
+					documentType = User.FIC;
+					break;
+				case 4:
+					documentType = User.PS;
+					break;
+				default:
+					System.out.println("Please type a correctly");
+				}
+			}while(optionAux!=1 &&optionAux!=2 &&optionAux!=3 &&optionAux!=4 );
+			
+			System.out.println("Type your document number");
+			documentNumber = s.next();
+			shiftControler.assignShift(documentNumber,documentType);
+			break;
+		case 3:
+			shiftControler.advanceShift();
+			
+		}
+		return continue1;
+	}
 }
+
+
